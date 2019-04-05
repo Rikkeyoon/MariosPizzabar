49ffcef795c239dbc5fcb82466a3a9602a0c969e
CarolineHoeg <49302236+CarolineHoeg@users.noreply.github.com>
2019-04-05 12:02:34 +0200

    Update Mario.sql

--- Mario.sql
+++ Mario.sql
@@ -1,34 +1,29 @@
-DROP TABLE  if exists Bestillingslinjer;
-DROP TABLE if exists Bestillinger;
-DROP TABLE if exists Pizzaer;
+DROP TABLE if exists bestillingslinjer;
+DROP TABLE if exists pizzaer;
+DROP TABLE if exists bestillinger;
 
+CREATE TABLE bestillinger (
+  bestilnr INT NOT NULL AUTO_INCREMENT,
+  afhenttid TIME NOT NULL,
+  PRIMARY KEY (bestilnr)
+  );
 
-CREATE TABLE  Bestillinger(	
-	Bestilnr INTEGER NOT NULL AUTO_INCREMENT,
-	Afhenttid TIME,
-	PRIMARY KEY (Bestilnr)
-); 
-
-CREATE TABLE Pizzaer(
-	Pizzanr INTEGER NOT NULL AUTO_INCREMENT,
-	Pizzanavn VARCHAR(50),
-    Toppings VARCHAR(255),
-    Pris FLOAT NOT NULL,
-	PRIMARY KEY (Pizzanr)
+CREATE TABLE pizzaer (	
+	pizzanr INT NOT NULL AUTO_INCREMENT, 
+	pizzanavn VARCHAR(50), 
+	toppings VARCHAR(255), 
+	pris FLOAT NOT NULL, 
+	PRIMARY KEY (pizzanr)
 );
 
-CREATE TABLE Bestillingslinjer(
-	Antal INTEGER NOT NULL,
-    Pizzanr INTEGER NOT NULL,
-    Bestilnr INTEGER NOT NULL,
-    PRIMARY KEY (Bestilnr, Pizzanr),
-	FOREIGN KEY (Bestilnr) REFERENCES Bestillinger(Bestilnr),
-	FOREIGN KEY (Pizzanr) REFERENCES Pizzaer(Pizzanr)
-);
-
-INSERT INTO bestillinger (afhenttid) VALUES ('18:30:00');
-INSERT INTO bestillinger (afhenttid) VALUES ('19:00:00');
-
+CREATE TABLE bestillingslinjer (
+  antal INT NOT NULL,
+  pizzanr INT NOT NULL,
+  bestilnr INT NOT NULL,
+  PRIMARY KEY (bestilnr, pizzanr),
+  FOREIGN KEY (bestilnr) REFERENCES bestillinger(bestilnr),
+  FOREIGN KEY (pizzanr) REFERENCES pizzaer(pizzanr)
+  );
 
 INSERT INTO pizzaer (pizzanavn, toppings, pris) VALUES ('Vesuvio', 'tomatsauce, ost, skinke og oregano', 57.0);
 INSERT INTO pizzaer (pizzanavn, toppings, pris) VALUES ('Amerikaner', 'tomatsauce, ost, oksefars og oregano', 53.0);
@@ -43,10 +38,13 @@ INSERT INTO pizzaer (pizzanavn, toppings, pris) VALUES ('Capricciosa', 'tomatsau
 INSERT INTO pizzaer (pizzanavn, toppings, pris) VALUES ('Hawaii', 'tomatsauce, ost, skinke, ananas og oregano', 61.0);
 INSERT INTO pizzaer (pizzanavn, toppings, pris) VALUES ('Le Blissola', 'tomatsauce, ost, skinke, rejer og oregano', 61.0);
 INSERT INTO pizzaer (pizzanavn, toppings, pris) VALUES ('Venezia', 'tomatsauce, ost, skinke, bacon og oregano', 61.0);
-INSERT INTO pizzaer (pizzanavn, toppings, pris) VALUES ('Mafia', 'tomatsauce, ost, pepperoni, bacon, løg og oregano', 61.0); 
+INSERT INTO pizzaer (pizzanavn, toppings, pris) VALUES ('Mafia', 'tomatsauce, ost, pepperoni, bacon, løg og oregano', 61.0);
 
-INSERT INTO bestillingslinjer (antal, pizzanr, bestilnr) VALUES (4, 5, 1);
-INSERT INTO bestillingslinjer (antal, pizzanr, bestilnr) VALUES (2, 6, 2);
+INSERT INTO bestillinger (afhenttid) VALUES ('18:30:00');
+INSERT INTO bestillinger (afhenttid) VALUES ('18:45:00');
+
+INSERT INTO bestillingslinjer VALUES (1, 5, 1);
+INSERT INTO bestillingslinjer VALUES (2, 12, 2);
 
 commit;
 
